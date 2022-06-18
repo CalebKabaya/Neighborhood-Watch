f410e1f88cf29122acee09e00ddbd6f73bf8d719
Moringa School <calebmutuku4@gmail.com>
2022-06-18 15:11:05 +0300

    ft:added the authentication urls

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns=[
     re_path('^$',views.index,name="index"),
+    re_path('login/',views.signin,name='login'),
+    re_path('register/',views.register,name='register'),
+    re_path('signout/',views.signout,name='signout'),
     
 ]
   
