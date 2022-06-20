b82b5336df94ffe2a83e4c8eb251a92d5abfeb57
Moringa School <calebmutuku4@gmail.com>
2022-06-20 19:50:15 +0300

    ft:added the urls for create post function

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,10 +15,25 @@ urlpatterns=[
     re_path(r'^joinhood/(?P<id>\d+)?$', views.join_hood, name='joinhood'), 
     re_path(r'^leavehood/(?P<id>\d+)?$', views.leave_hood, name='leavehood'), 
     re_path(r'^viewhood/(?P<hood_id>\d+)?$', views.viewhood, name='viewhood'), 
-    # re_path(r'^newpost/(?P<hood_id>\d+)?$', views.create_post, name='newpost'),
+    # re_path(r'^addpost/(?P<hood_id>\d+)?$', views.add_post, name='addpost'), 
+
+    # re_path(r'^newpost/(?P<hood_id>\d+)?$', views.add_post, name='newpost'),
+    re_path(r'^addpost/$', views.new_post, name='addpost'),
+
+
     # re_path('newpost/<hood_id>', views.create_post, name='newpost'),
-    re_path('newpost/<int:hood_id>',views.create_post,name='newpost'),
+    # re_path(r'^newpost/(?P<hood_id>\d+)?$', views.addpost, name='newpost'), 
+
+    # re_path('newpost/',views.create_post,name='newpost'),
+    # re_path('add_post_comment/<int:post_id>',views.add_post_comment,name='add_post_comment'),
+
         # path('<hood_id>/members', views.hood_members, name='members'),
+    # re_path('createpost/',views.create_post,name='createpost'),
+    # re_path(r'^newpost$', views.new_post, name='newpost'),
+    # re_path('newpost', views.create_post, name='newpost'),
+
+    # re_path(r'^newpost/<int:hood_id>',views.add_post,name='newpost'),
+
     re_path(r'^members/(?P<hood_id>\d+)?$', views.hood_members, name='members'), 
     re_path('search/', views.search_business, name='search'),
 
