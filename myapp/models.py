41ae4b73502e3a85104947e16e9d129ef5d21ee8
Moringa School <calebmutuku4@gmail.com>
2022-06-20 19:50:46 +0300

    eddited the post model

--- myapp/models.py
+++ myapp/models.py
@@ -79,5 +79,15 @@ class Post(models.Model):
     image= CloudinaryField('image')
     post = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='post_owner')
+    user = models.ForeignKey(User, on_delete=models.CASCADE,)
     hood = models.ForeignKey(NeighbourHood, on_delete=models.CASCADE, related_name='hood_post' )
+
+    def __str__(self):
+        return f'{self.title}Post'
+    
+    def create_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()    
+
